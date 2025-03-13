73c9ba68ebf5010128a86811c69fdad0d6b43cef
Vinay14Newbie <wjust1984@gmail.com>
2025-03-13 10:03:01 +0800

    some changes

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,9 +1,16 @@
 import mysql from 'mysql2';
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from './serverConfig.js';
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER
+} from './serverConfig.js';
 
 export const db = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_NAME
+  database: DB_NAME,
+  port: DB_PORT
 });
